c352d7c2711aa6e420bed56e92814e1b673ff34c
Ben reeves <72570746+UP957029@users.noreply.github.com>
2025-09-25 10:36:21 +0100

    Made the timer into a class to eliminate need for global variables and make the timer accesable for future work.

--- main.py
+++ main.py
@@ -13,77 +13,83 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
-def timer_reset():
-    window.after_cancel(timer)
-    canvas.itemconfig(timer_text, text="00:00")
-    title_label.config(text="Timer", fg=GREEN)
-    check_mark.config(text="")
-    global reps
-    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-def start_timer():
-    global reps
-    reps += 1
+class PomodoroTimer:
+    def __init__(self):
+        self.reps = 0
+        self.timer = None
 
-    work_sec = 1 * 60
-    short_break_sec = 1 * 60
-    long_break_seconds = LONG_BREAK_MIN * 60
+        self.window = Tk()
+        self.window.title("Pomodoro")
+        self.window.config(padx=100, pady=50, bg=YELLOW)
 
-    if reps % 8 == 0:
-        count_down(long_break_seconds)
-        title_label.config(text="Long Break", fg= RED )
-    elif reps % 2 == 0:
-        count_down(short_break_sec)
-        title_label.config(text="Short Break", fg=PINK)
-    else:
-        count_down(work_sec)
-        title_label.config(text="Work Time", fg=GREEN)
+        self.canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+        self.img = PhotoImage(file="tomato.png")
+        self.canvas.create_image(103, 112, image=self.img)
+        self.timer_text = self.canvas.create_text(103, 130, text="00.00", fill="white", font=(FONT_NAME, 35, "bold"))
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(count):
-    count_min = floor(count / 60)
-    count_sec = count % 60
+        self.title_label = Label(text="Timer")
+        self.title_label.config(fg=GREEN, font=(FONT_NAME, 28, "bold"), bg=YELLOW)
+        self.title_label.grid(column=1, row=0)
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02}")
-    if count > 0:
-        global timer
-        timer = window.after(1000, count_down, count - 1)
-    else:
-        start_timer()
-        mark = ""
-        completed_work_sessions =  floor(reps / 2)
-        for i in range (completed_work_sessions):
-            mark += "✓"
-        check_mark.config(text = mark)
+        self.check_mark = Label()
+        self.check_mark.config(fg=GREEN, bg=YELLOW)
+        self.check_mark.grid(column=1, row=3)
 
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+        self.start_btn = Button(text="Start", highlightthickness=0, command=self.start_timer)
+        self.start_btn.grid(column=0, row=2)
 
-canvas = Canvas(width= 200, height= 224, bg= YELLOW, highlightthickness= 0)
-img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=img)
-timer_text = canvas.create_text(103, 130, text= "00.00", fill="white", font=(FONT_NAME, 35, "bold"))
+        self.reset_btn = Button(text="Reset", highlightthickness=0, command=self.timer_reset)
+        self.reset_btn.grid(column=2, row=2)
 
-title_label = Label(text="Timer")
-title_label.config(fg=GREEN, font=(FONT_NAME, 28, "bold"), bg=YELLOW)
-title_label.grid(column=1, row = 0)
+        self.canvas.grid(column=1, row=1)
 
-check_mark = Label()
-check_mark.config(fg=GREEN, bg=YELLOW)
-check_mark.grid(column=1, row=3)
+        self.window.mainloop()
+    # ---------------------------- TIMER RESET ------------------------------- #
+    def timer_reset(self):
+        self.window.after_cancel(timer)
+        self.canvas.itemconfig(self.timer_text, text="00:00")
+        self.title_label.config(text="Timer", fg=GREEN)
+        self.check_mark.config(text="")
+        self.reps = 0
 
-start_btn = Button(text="Start", highlightthickness=0, command=start_timer)
-start_btn.grid(column=0, row=2)
+    # ---------------------------- TIMER MECHANISM ------------------------------- #
+    def start_timer(self):
 
-reset_btn = Button(text="Reset", highlightthickness=0, command=timer_reset)
-reset_btn.grid(column=2, row=2)
+        self. reps += 1
 
-canvas.grid(column=1, row=1)
+        work_sec = 1 * 60
+        short_break_sec = 1 * 60
+        long_break_seconds = LONG_BREAK_MIN * 60
 
+        if self.reps % 8 == 0:
+            self.count_down(long_break_seconds)
+            self.title_label.config(text="Long Break", fg= RED )
+        elif self.reps % 2 == 0:
+            self.count_down(short_break_sec)
+            self.title_label.config(text="Short Break", fg=PINK)
+        else:
+            self.count_down(work_sec)
+            self.title_label.config(text="Work Time", fg=GREEN)
 
+    # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+    def count_down(self, count):
+        count_min = floor(count / 60)
+        count_sec = count % 60
 
-window.mainloop()
+        self.canvas.itemconfig(self.timer_text, text=f"{count_min}:{count_sec:02}")
+        if count > 0:
+            global timer
+            timer = self.window.after(1000, self.count_down, count - 1)
+        else:
+            self.start_timer()
+            mark = ""
+            completed_work_sessions =  floor(self.reps / 2)
+            for i in range (completed_work_sessions):
+                mark += "✓"
+            self.check_mark.config(text = mark)
+
+    # ---------------------------- UI SETUP ------------------------------- #
+
+if __name__ == "__main__":
+    PomodoroTimer()
